22b441e9a85ba4daab1a5b1a22b291f51c11254d
Ali Rouhi <alirhtg@gmail.com>
2024-04-16 20:50:45 +0330

    adding search box

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react"
-import { books } from "../constants/mockData.js"
+import { books as bookData } from "../constants/mockData.js"
 import BookCard from "./BookCard.jsx"
+import SideCard from "./SideCard.jsx"
+
+import styles from "./Books.module.css"
+import SearchBox from "./SearchBox.jsx"
+
 
 function Books() {
+    const [books, setBooks] = useState(bookData)
     const [liked, setLiked] = useState([])
+    const [search, setSearch] = useState([])
+
     const handleLikedList = (book, status) => {
         if (status) {
             const newLikedList = liked.filter(i => i.id != book.id); setLiked(newLikedList)
@@ -11,14 +19,26 @@ function Books() {
             setLiked(liked => [...liked, book])
         }
     }
-    return (
-        <div>
-            <div>{ books.map(book => (
-                <BookCard key={ book.id } data={ book } handleLikedList={ handleLikedList } />)) }
-            </div>
-            <div>{!!liked.length && <div></div>}</div>
 
-        </div>
+    const searchHandler = () => {
+        if (search) {
+            const newBooks = bookData.filter(book => book.title.toLowerCase().includes(search))
+            setBooks(newBooks)
+        } else {
+            setBooks(bookData)
+        }
+    }
+
+    return (
+        <>
+            <SearchBox search={ search } setSearch={ setSearch } searchHandler={searchHandler} />
+            <div className={ styles.container }>
+                <div className={ styles.cards }>{ books.map(book => (
+                    <BookCard key={ book.id } data={ book } handleLikedList={ handleLikedList } />)) }
+                </div>
+                { !!liked.length && <div className={ styles.favorite }><h4>Favorites</h4>{ liked.map(book => <SideCard key={ book.id } data={ book } />) }</div> }
+
+            </div></>
     )
 }
 
